7898802a2566513609736e5f088fdb899fba5456
davydebut <spanudavy@gmail.com>
2022-06-08 19:31:04 +0200

    Admin réussi

--- src/Controller/Admin/ActorCrudController.php
+++ src/Controller/Admin/ActorCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Actor;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ActorCrudController extends AbstractCrudController
